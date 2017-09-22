0f25bc059a8af1e23898a8a6f6e1401ecb7dc3e6
Abby Christens <achristens@gmail.com>
2017-09-22 16:01:32 -0400

    Creating three attributes for Cat class

--- cat_project/cat.rb
+++ cat_project/cat.rb
@@ -1,3 +1,11 @@
 class Cat
-
+  def initialize(name, preferred_food, meal_time)
+    @name = name
+    @preferred_food = preferred_food
+    @meal_time = meal_time
+  end
 end
+
+squish = Cat.new("Squish", "Fish", 12)
+
+puts squish
