75e2592bca33ba4cbcadae205b69ed17d58ccadd
Abby Christens <achristens@gmail.com>
2017-09-22 18:39:45 -0400

    Adding treasure method and levelling up

--- player.rb
+++ player.rb
@@ -1,6 +1,6 @@
 class Player
   def initialize
-    @gold_coins = 0
+    @gold_coins = 9
     @health_points = 10
     @lives = 5
   end
@@ -9,6 +9,22 @@ class Player
     @lives += 1
     "You now have #{@lives} lives remaining."
   end
+
+  def collect_treasure
+    @gold_coins += 1
+    if (@gold_coins % 10 == 0)
+      print "You leveled up! You now have #{@gold_coins} gold coins PLUS "
+      level_up
+    else
+      "You now have #{@gold_coins} gold coins."
+    end
+  end
+
+  def do_battle(damage)
+    @health_points = @health_points - damage
+    "Your health level is at #{@health_points} points."
+  end
 end
 
 abby = Player.new
+puts abby.collect_treasure
