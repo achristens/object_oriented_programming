7b86fde19bec650b2c87b470f652d7f56355e1c7
Abby Christens <achristens@gmail.com>
2017-09-24 15:59:01 -0400

    Adding report method

--- paperboy_project/paperboy.rb
+++ paperboy_project/paperboy.rb
@@ -18,78 +18,35 @@ class Paperboy
   end
 
   def quota
-    number = 50 + (@experience / 2)
+    50 + (@experience / 2)
   end
 
   def deliver(start_address, end_address)
-    total_papers = (end_address - start_address) + 1
-    quota_papers = quota * 0.25
-    quota_papers = quota_papers.to_f
-    bonus_papers = (total_papers - quota) * 0.50
-    bonus_papers = bonus_papers.to_f
-    total_earned = quota_papers + bonus_papers
-    @experience = total_papers
+    total_addresses = (end_address - start_address).abs
+    if total_addresses == quota
+      quota_papers = quota * 0.25
+      quota_papers = quota_papers.to_f
+      days_total = quota_papers
+      @earnings += days_total
+
+    elsif total_addresses > quota
+      quota_papers = quota * 0.25
+      quota_papers = quota_papers.to_f
+      bonus_papers = (total_addresses - quota) * 0.5
+      bonus_papers = bonus_papers.to_f
+      days_total = quota_papers + bonus_papers
+      @earnings += days_total
+
+    else
+      @earnings -= 2
+    end
+    @experience += total_addresses
+    @earnings
+  end
+
+  def report
+    "I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
   end
 end
 
 ari = Paperboy.new("Ari")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# class Paperboy
-#   def initialize(name)
-#     @name       = name
-#     @experience = 0
-#     @earnings   = 0
-#   end
-#
-#   def name
-#     @name
-#   end
-#
-  def experience
-    @experience
-  end
-
-  def earnings
-    @earnings
-  end
-
-  def quota
-    # If first delivery, quota = 50
-    number = 50 + (@experience / 2)
-    "Your quota is #{number} papers to deliver."
-  end
-#
-# end
-#
-# abby = Paperboy.new("Abby")
